612e58b8b76f4d540c1da351ef6daa4ddb4e016e
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2023-06-22 09:47:24 +0300

    checks for capital letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ *  _isupper: checks is the letter is capital letter
+ *  &c: is parameter to checked
+ *
+ *  Return: 1 for capital letter and 0 for other
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
