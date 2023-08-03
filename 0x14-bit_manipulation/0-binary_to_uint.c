fa9334e6a22011494f4257b91cf3d738ebe551bd
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2023-08-03 18:00:10 +0300

    bit manuplation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,25 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, result = 0;
-	int i = 0, j = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (*(b + len) != '\0')
-		len++;
-
-	for (i = (len - 1), j = 0; i >= 0; i--, j++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		result += (b[i] - '0') * _pow_recursion(2, j);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
-}
-/**
- * _pow_recursion - Function that returns the factorial of a given number
- * @x: Base
- * @y: Power
- *
- * Return: Result of x raised to the power of y.
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y < 0)
-		return (-1);
-	return (x * _pow_recursion(x, y - 1));
+	return (dec_val);
 }
