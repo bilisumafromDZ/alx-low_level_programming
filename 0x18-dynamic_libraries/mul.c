1601b31ff6c254424b3904f4c9a8607933d84d20
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2023-11-13 18:11:32 +0300

    c dynamic library used in python

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - mul two integers
+ * @a: first int
+ * @b: second int
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
